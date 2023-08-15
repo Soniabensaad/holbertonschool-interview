1a21d8d3178849a3884b650c41eb6899c981516b
sonia <https://github.com/Soniabensaad>
2023-08-15 14:38:05 +0200

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
 """method that determines if all the boxes can be opened."""
+
+
 def canUnlockAll(boxes):
     num_boxes = len(boxes)
     unlocked = [False] * num_boxes
     unlocked[0] = True
-    locked = [0] 
+    locked = [0]
 
     while locked:
         current = locked.pop()
         for key in boxes[current]:
             if 0 <= key < num_boxes and not unlocked[key]:
                 unlocked[key] = True
-                locked.append(key)  
-    
+                locked.append(key)
     return all(unlocked)
