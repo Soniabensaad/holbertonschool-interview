a13976b0914b251ab4c16634b6958cbe8eafea46
sonia <https://github.com/Soniabensaad>
2023-08-23 22:12:03 +0200

    insert in sorted linked list

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -4,14 +4,15 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node = malloc(sizeof(listint_t));
+    listint_t *new_node;
+    listint_t *current;
+
+    new_node = malloc(sizeof(listint_t));
     if (new_node == NULL)
         return NULL;
 
     new_node->n = number;
     new_node->next = NULL;
-
-    listint_t *current;
 
     if (*head == NULL || number < (*head)->n)
     {
