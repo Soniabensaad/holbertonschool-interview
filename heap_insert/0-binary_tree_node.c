e445af33ebc57b8ffd8f31c9ea5cb76aec6839ef
sonia <https://github.com/Soniabensaad>
2023-09-01 13:18:34 +0200

    tree

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include "binary_trees.h"
 
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
 binary_tree_t *node = malloc(sizeof(binary_tree_t));
     if (node) {
