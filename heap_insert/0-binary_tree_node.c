f096d7ec79631cbc212b4e226d15f2ae23cd07c0
sonia <https://github.com/Soniabensaad>
2023-09-01 13:28:30 +0200

    tree

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -11,7 +11,8 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
 binary_tree_t *node = malloc(sizeof(binary_tree_t));
 if (node)
 {
