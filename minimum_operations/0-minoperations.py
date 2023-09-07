8547fc8a230680eacd5fdb4a61d1104a1986c74f
sonia <https://github.com/Soniabensaad>
2023-09-07 17:32:17 +0200

    interview

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,18 +3,16 @@
 Minimum Operations
 """
 def minOperations(n):
-    
     if n <= 1:
         return 0
+
     o = 0
     d = 2
+
     while n > 1:
         while n % d == 0:
-            o += 2
-            n //= d  
+            o += d
+            n //= d
         d += 1
+
     return o
-
-
-        
-       
