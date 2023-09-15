72fa43dac28637c4c0fc2d6c20f95f2ee73df1aa
sonia <https://github.com/Soniabensaad>
2023-09-15 23:25:30 +0200

    JQuery_advanced

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,79 +1,108 @@
-/* 0-sandpiles.c */
+#include "sandpiles.h"
 
-#include <stdio.h>
+/**
+ * print_grid - prints a 3x3 grid
+ * @grid: the grid
+ */
 
-void print_grid(int grid[3][3]) {
+static void print_grid(int grid[3][3])
+{
     int i, j;
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("%d", grid[i][j]);
-            if (j < 2) {
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
                 printf(" ");
-            }
+            printf("%d", grid[i][j]);
         }
         printf("\n");
     }
 }
 
-void topple(int grid[3][3], int x, int y) {
-    grid[x][y] -= 4;
-    if (x > 0) grid[x - 1][y]++;
-    if (x < 2) grid[x + 1][y]++;
-    if (y > 0) grid[x][y - 1]++;
-    if (y < 2) grid[x][y + 1]++;
+/**
+ * cpy - copies grid1 to grid2
+ * @grid1: grid1
+ * @grid2: grid2
+ */
+
+static void cpy(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            grid2[i][j] = grid1[i][j];
 }
 
-void sandpiles_sum(int grid1[3][3], int grid2[3][3]) {
-    int result[3][3];
-    int i, j; /* Move variable declarations to the beginning */
+/**
+ * topple - topples the values in the grid
+ * @grid1: Grid to topple
+ * @grid2: Using to copy for topple check
+ */
 
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            result[i][j] = grid1[i][j] + grid2[i][j];
+static void topple(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+
+    cpy(grid1, grid2);
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (grid2[i][j] > 3)
+            {
+                grid1[i][j] -= 4;
+                if (j - 1 > -1)
+                    grid1[i][j - 1] += 1;
+                if (j + 1 < 3)
+                    grid1[i][j + 1] += 1;
+                if (i - 1 > -1)
+                    grid1[i - 1][j] += 1;
+                if (i + 1 < 3)
+                    grid1[i + 1][j] += 1;
+            }
         }
     }
+}
 
-    while (1) {
-        int unstable = 0;
+/**
+ * is_stable - Determines if grid is stable
+ * @grid1: checks if >3
+ * Return: 0 if stable, 1 if unstable
+ */
 
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 3; j++) {
-                if (result[i][j] > 3) {
-                    unstable = 1;
-                    break;
-                }
-            }
-            if (unstable) break;
-        }
+int is_stable(int grid1[3][3])
+{
+    int i, j;
 
-        /* If no unstable cells, exit the loop */
-        if (!unstable) break;
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            if (grid1[i][j] > 3)
+                return (1);
+    return (0);
+}
 
-        /* Topple unstable cells */
-        int temp[3][3];
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 3; j++) {
-                temp[i][j] = result[i][j];
-            }
-        }
+/**
+ * sandpiles_sum - Computes sum of two sandpiles
+ * @grid1: First grid 3x3 grid (0-3)
+ * @grid2: Second 3x3 grid (0-3)
+ */
 
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 3; j++) {
-                if (temp[i][j] > 3) {
-                    topple(result, i, j);
-                }
-            }
-        }
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    int i, j, s;
 
-        if (unstable) {
-            print_grid(result);
-            printf("=\n");
-        }
-    }
-
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            grid1[i][j] = result[i][j];
-        }
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            grid1[i][j] += grid2[i][j];
+    s = is_stable(grid1);
+    while (s)
+    {
+        printf("=\n");
+        print_grid(grid1);
+        topple(grid1, grid2);
+        s = is_stable(grid1);
     }
 }
