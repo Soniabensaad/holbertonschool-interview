fe384a3c8a3dc67682ab443feeddc3feb206bfba
sonia <https://github.com/Soniabensaad>
2023-09-22 19:58:43 +0200

    python async

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -15,25 +15,28 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-// Function to check if a linked list is a palindrome
+/* Function to check if a linked list is a palindrome */
 int is_palindrome(listint_t **head) {
-    if (*head == NULL || (*head)->next == NULL) {
-        return 1; // An empty list or a single-node list is considered a palindrome
-    }
-
-    listint_t *slowPtr = *head;
-    listint_t *fastPtr = *head;
+    listint_t *slowPtr;
+    listint_t *fastPtr;
     listint_t *secondHalf = NULL;
     listint_t *prev = NULL;
 
-    // Find the middle of the linked list using the slow and fast pointers
+    if (*head == NULL || (*head)->next == NULL) {
+        return 1; /* An empty list or a single-node list is considered a palindrome */
+    }
+
+    slowPtr = *head;
+    fastPtr = *head;
+
+    /* Find the middle of the linked list using the slow and fast pointers */
     while (fastPtr != NULL && fastPtr->next != NULL) {
         fastPtr = fastPtr->next->next;
         prev = slowPtr;
         slowPtr = slowPtr->next;
     }
 
-    // Reverse the second half of the linked list
+    /* Reverse the second half of the linked list */
     prev->next = NULL;
     while (slowPtr != NULL) {
         listint_t *next = slowPtr->next;
@@ -42,17 +45,35 @@ int is_palindrome(listint_t **head) {
         slowPtr = next;
     }
 
-    // Compare the first half with the reversed second half
+    /* Compare the first half with the reversed second half */
     listint_t *firstHalf = *head;
     while (firstHalf != NULL && secondHalf != NULL) {
         if (firstHalf->n != secondHalf->n) {
-            return 0; // Not a palindrome
+            return 0; /* Not a palindrome */
         }
         firstHalf = firstHalf->next;
         secondHalf = secondHalf->next;
     }
 
-    return 1; // It's a palindrome
+    return 1; /* It's a palindrome */
 }
 
 // Rest of your code, including the main function and other utility functions
+// Function to insert a new node at the end of the linked list
+void insertEnd(listint_t** head, int data) {
+    listint_t* newNode = (listint_t*)malloc(sizeof(listint_t));
+    newNode->n = data;
+    newNode->next = NULL;
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    listint_t* current = *head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = newNode;
+}
